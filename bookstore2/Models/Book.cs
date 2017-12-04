df8050b633ea048bf1a757b84d343c58c0adb618
Гаврилов Антон Дмитриевич <gavranton@gmail.com>
2017-12-04 10:41:58 +0300

    llll

--- bookstore2/Models/Book.cs
+++ bookstore2/Models/Book.cs
@@ -24,5 +24,6 @@ namespace bookstore2.Models
         public int Year5 { get; set; }
         public int Year6 { get; set; }
         public int Year7 { get; set; }
+        public int Year8 { get; set; }
     }
 }
